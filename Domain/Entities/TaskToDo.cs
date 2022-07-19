e78664acb5a49ab888aa1634f6ae157007fd0e1b
Mario Neto <marioalvesneto@hotmail.com>
2022-07-19 01:54:24 -0300

    first commit

---
+++ Domain/Entities/TaskToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class TaskToDo : IIdentityEntity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime DeadLine { get; set; }
+
+        public bool Status { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ Infrastructure/DBConfiguration/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DBConfiguration
+{
+    public class DatabaseConnection
+    {
+        public static IConfiguration ConnectionConfiguration
+        {
+            get
+            {
+                IConfigurationRoot Configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("DBConfiguration/appsettings.json")
+                    .Build();
+                return Configuration;
+            }
+        }
+    }
+}
