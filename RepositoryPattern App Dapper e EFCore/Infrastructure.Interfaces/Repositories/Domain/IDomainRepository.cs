ab27b8110d07dd8157fa24a299e38149a34f7f79
Mario Neto <marioalvesneto@hotmail.com>
2022-07-20 02:54:02 -0300

    Infrastructure interfaces

---
+++ RepositoryPattern App Dapper e EFCore/Infrastructure.Interfaces/Repositories/Domain/IDomainRepository.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Interfaces.Repositories.Standard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Interfaces.Repositories.Domain
+{
+    public interface IDomainRepository<TEntity> : IRepositoryAsync<TEntity> where TEntity : class, IIdentityEntity
+    {
+    } 
+}
